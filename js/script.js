6417bbff14aa04b26244323a06667bc6ad770861
aliaa99511 <64794795+aliaa99511@users.noreply.github.com>
2021-08-13 19:23:40 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+function spinner() {
+    var spinner=document.getElementById("spinner");
+
+    setTimeout(function() {
+        spinner.style.display = "none";
+    }, 2000);
+}
+
+spinner();
